e8ce1386a26395574e90d46f8ce76dda7b9df2ec
Ali Haider <149256431+alih295@users.noreply.github.com>
2025-11-16 20:42:06 +0500

    Update layout and styles in HerSection component
    Refactor HerSection component layout and styling.

--- src/Components/HerSection.jsx
+++ src/Components/HerSection.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 function HerSection() {
    return (
-      <div className='w-full lg:gap-0 h-screen  gap-10 z-10 relative lg:px-10 lg:mt-40  flex  lg:flex-row flex-col items-center justify-center lg:justify-center mt-10 font-[myFont]'>
+      <div className='w-full lg:gap-0   gap-10 z-10 relative lg:px-10 lg:mt-40  flex  lg:flex-row flex-col items-center justify-center mt-10 font-[myFont]'>
          <div className='lg:w-[50vw] w-full lg:h-[500px]  px-5 rounded-lg flex items-start justify-center flex-col'>
             <p className='lg:text-2xl text-lg tracking-tight lg:leading-[3.5vw] '>
                I'm <span className='lg:text-[3vw] text-2xl font-bold  m-2 font-[myFont] text-gray-200  '> Ali Haider</span> , a <span className='lg:text-[3vw] text-4xl strokedText m-2 font-[myfont2] text-teal-500 lg:text-transparent  '> MERN Stack Developer</span>   specializing in creating modern, high-performance web solutions that help businesses grow online.
@@ -16,7 +16,15 @@ function HerSection() {
          </div>
 
 
-         <div className='lg:w-[40vw] w-full hover:border-2 border-dashed transition-all duration-200 shadow-lg shadow-gray-700  overflow-hidden rounded-full  flex items-center justify-center'>
+         <div className='  w-full 
+   lg:w-[40vw] 
+   overflow-hidden
+   flex items-center justify-center
+   border-dashed hover:border-2
+   shadow-lg shadow-gray-700
+   rounded-xl lg:rounded-full
+   lg:h-[450px]
+   h-[250px]'>
             <img className=' w-full h-full  object-cover' src='/img.png' alt="img is not showing" />
          </div>
 
